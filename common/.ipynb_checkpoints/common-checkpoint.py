5c65be71028bfd76b7fc2b5d209c1171095e917a
Chintan Gandhi <c.gandhi@f5.com>
2025-02-16 18:31:53 -0800

    Update

--- common/.ipynb_checkpoints/common-checkpoint.py
+++
@@ -1,5 +0,0 @@
-from sklearn.metrics import accuracy_score, recall_score, precision_score
-
-def precision_thresh(predict_probs, y_test, thresh):
-    preds = np.where(predict_probs >= thresh, 'yes', 'no')    
-    return precision_score(y_test, preds, pos_label='yes')
